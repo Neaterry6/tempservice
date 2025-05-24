26970a6911fe4b4542010ccff6e29c35e0e90bcc
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-05-24 17:04:50 +0100

    Update recievesms.py

--- recievesms.py
+++ recievesms.py
@@ -1,9 +1,10 @@
+
 import requests
 
 RECEIVESMS_API_URL = "https://receive-smss.com/free-phone-number"
 
 def get_free_number():
-    """Fetches a free temp number from Receive-SMS."""
+    """Fetches a free temporary phone number from Receive-SMS."""
     response = requests.get(RECEIVESMS_API_URL)
     if response.status_code == 200:
         data = response.json()
@@ -11,9 +12,9 @@ def get_free_number():
     return "Failed to get phone number"
 
 def receive_sms(number):
-    """Fetch SMS messages for the temp number."""
+    """Fetches SMS messages for the temporary phone number."""
     inbox_api_url = f"https://receive-smss.com/get-messages?number={number}"
     response = requests.get(inbox_api_url)
     if response.status_code == 200:
         return response.json().get("messages", [])
-    return "Failed to fetch messages"  
+    return "Failed to fetch messages"
